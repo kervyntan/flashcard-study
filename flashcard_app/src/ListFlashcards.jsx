08b7dc1208cffa3e29d211e14b21cadb9765dc6f
Kervyn <kervyntan@gmail.com>
2023-03-09 23:19:19 +0800

    added onSnapshot for Listflashcards

--- flashcard_app/src/ListFlashcards.jsx
+++ flashcard_app/src/ListFlashcards.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { db } from '../firebase';
-import { collection, getDocs } from 'firebase/firestore';
+import { Card, Text } from '@mantine/core';
+import { collection, getDocs, onSnapshot } from 'firebase/firestore';
 
 const ListFlashcards = () => {
+    const dbRef = collection(db, "flashcards");
+    const [flashcards, setFlashcards] = useState([]);
+    useEffect ( () => {
+        onSnapshot(dbRef, docs => {
+            // console.log(docs);
+            docs.forEach(doc => {
+                console.log("Hi");
+                setFlashcards((flashcards) => [...flashcards, doc]);
+            })
+        });
+    }, []);
+
+    const displayFlashcards = flashcards.map((item, index) => {
+        return (
+            <Card className="flashcard-item" shadow="sm" padding="lg" radius="md" withBorder>
+                <Text mt="md" size="sm" color="dimmed">
+                    {item.text}
+                </Text>
+            </Card>
+        )
+    })
+
+    return (
+        <div className="list-flashcards">
+            {displayFlashcards}
+        </div>
+    )
+
 
 }
 
